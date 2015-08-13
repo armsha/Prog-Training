df6e911be1fa972d6101a2407d3ce2d40e53ef65
dali <dali@itchy.cs.umu.se>
2015-08-13 10:00:55 +0200

    Initiated file for bipartite matching

---
+++ Implementerat/Training/Suites/BiMa.cpp
@@ -0,0 +1,58 @@
+/*
+ * Algorithm implementation training test suite.
+ *	NAME: <template>
+ *	INPUT: <input for algorithm>
+ *	OUTPUT: <output for algorithm>
+ *	DESCRIPTION: <what the algorithm does>
+ *	USE: <where the algorithm is used, or an example>
+ *	COMPLEXITY: <a solution of atleast O(x) exists...>
+ */
+
+// Includes and namespace
+
+/*
+ * Method to implement
+ *	DESCRIPTION: <detailed description of method>
+ *	INPUT: <@param stuff>
+ *	OUTPUT: <@return stuff>
+ */	
+// -- Implementation here ---
+
+
+
+/*
+ * Results to enter when done.
+ *	TIME: <how many minutes did it take this time>
+ *	COMPLEXITY: <what O is the implementation>
+ */
+
+/* ------ Test Section ------ */
+#ifdef __TEST__
+
+// Includes
+#include "tt_test.h"
+
+/* Test methods */
+// void testmethod(){
+// 	TT_assert_True( true );
+// 	TT_assert_EQ( 2, 1+1 );
+// 	TT_FAIL;
+// }
+
+
+/* 
+ * Main method
+ * Runs all the tests, and prints the result
+ */
+int main(int argc, char const *argv[])
+{
+
+	TT_SETUP;
+	/* Tests */
+	// TT_TEST(testmethod);
+	TT_FINAL;
+
+	return 0;
+}
+
+#endif
